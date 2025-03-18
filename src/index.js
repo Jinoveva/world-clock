066dd8dca0546110fe6bd2f5a5e29abcf2bbf897
Jinoveva <jinovevalopes@gmail.com>
2025-03-18 21:52:42 +0200

    Added functionality

--- src/index.js
+++ src/index.js
@@ -14,29 +14,42 @@ function showTime() {
   sydneyTimeElement.innerHTML = sydney.format("h:mm:ss [<small>]A[</small>]");
 }
 
+showTime();
+setInterval(showTime, 1000);
+
+// To store the interval ID so we can clear it when a new city is selected
+let cityInterval;
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
-  </div>
-  `;
-}
 
-showTime();
-setInterval(showTime, 1000);
+  function updateTime() {
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+      </div>
+      <a href=''>All cities</a>
+    `;
+  }
+
+  updateTime();
+
+  // Clear any previous interval before setting a new one
+  clearInterval(cityInterval);
+  cityInterval = setInterval(updateTime, 1000);
+}
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
